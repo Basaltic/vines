f640207b6fda0b31273025a951ca22aebe11d9ce
Lei Chen <lei91122@gmail.com>
2025-06-16 22:18:27 +0800

    Update app-route-tree.ts

--- apps/desktop/src/app/router/app-route-tree.ts
+++ apps/desktop/src/app/router/app-route-tree.ts
@@ -1,6 +1,9 @@
-import { boardRoute } from '@/features/board/route';
+// import { boardRoute } from '@/features/board/route';
 import { rootRoute } from './app-root-route';
 import { landingRoute } from '@/features/start-up/landing/route';
-import { firstUseSetupPageRoute } from '@/features/start-up/first-use-setup/route';
+// import { firstUseSetupPageRoute } from '@/features/start-up/first-use-setup/route';
 
-export const routeTree = rootRoute.addChildren([landingRoute, firstUseSetupPageRoute, boardRoute]);
+export const routeTree = rootRoute.addChildren([
+    landingRoute,
+    // , firstUseSetupPageRoute, boardRoute
+]);
